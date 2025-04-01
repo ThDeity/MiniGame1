dc6039f021e2922d2ef523016ed623e3ec3ec3c9
ThDeity <109583076+ThDeity@users.noreply.github.com>
2025-04-01 19:50:42 +0300

    Pause
    Pause was added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndGame : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     [SerializeField] protected GameObject _panel;
 
     protected virtual void OnTriggerEnter2D(Collider2D collision) => _panel.SetActive(true);
 
     public void Restart() => SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    public void Pause() => Time.timeScale = Time.timeScale == 0 ? 1 : 0;
 }
